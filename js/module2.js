f75e3dff044f6fbaad5a5612db2b03a1fe69dbe9
Vlad Gonchar <vladik.gonchar2015@gmail.com>
2021-05-27 23:38:25 +0300

    Дз2

---
+++ js/module2.js
@@ -0,0 +1,32 @@
+// Task 1
+// function checkAge(age) {
+//   if (age >= 18) { // Change this line
+//     return "You are an adult";
+//   }
+
+//   return "You are a minor";
+// }
+
+// Task 2
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   // Change code below this line
+//   if (password === ADMIN_PASSWORD) {
+//     return "Welcome!";
+//   } 
+//   return "Access denied, wrong password!"
+//   // Change code above this line
+// }
+
+// Task 3
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+//   if (ordered === 0) {
+//     return "Your order is empty!";
+//   } 
+//   if (ordered > available) {
+//     return "Your order is too large, not enough goods in stock!";
+//   } 
+//   return "The order is accepted, our manager will contact you";;
+//   // Change code above this line
+// }
